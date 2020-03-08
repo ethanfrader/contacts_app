3e421b564d6cc6d6d01de8fa01bc6e33c43d1c45
Ethan Rader <rader.ethan@gmail.com>
2020-03-08 11:47:25 -0500

    added models for friendly updated at and for full name

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%l:%M%P %B %e, %Y")
+  end
+
+  def full_name
+    full = ""
+    full += "#{first_name} #{last_name}"
+  end
 end
